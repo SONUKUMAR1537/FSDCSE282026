e57c0c7115b8c9a6ead804d9be3ffe9e0bb27567
SONUKUMAR1537 <sonukumarb1537@gmail.com>
2026-07-27 15:37:19 +0530

    app

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("helllo i am using js "); // console.log
+// // console.log("helllo i am using js "); // console.log
+
+// // function sum(a,b){
+// //     return a+b;
+
+// // }
+// // sum(23,30);
+// // console.log( sum(23,30));
+
+// var a=34;
+// if(a>20){
+//     var=45;
+//     console.log("a inside a ")
+// }
