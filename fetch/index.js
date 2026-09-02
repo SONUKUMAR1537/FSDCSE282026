100c729ddada6ce93126b6509db44b2f4841c7ab
SONUKUMAR1537 <sonukumarb1537@gmail.com>
2026-09-02 10:23:07 +0530

    react

--- fetch/index.js
+++ fetch/index.js
@@ -2,6 +2,8 @@ const root=document.getElementById('container')
 console.log(root);
 const button=document.getElementById('btn')
 console.log(button);
+
+
 async function getData(){
     // alert("hii")
  const serverData= await  fetch('https://fakestoreapi.com/products')
